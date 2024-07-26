1812f4ed2a0a918729ccad2361e06bbbe6b9e5a3
Motjiang <motjiangbrilliant@gmail.com>
2024-07-26 13:54:55 +0200

    added the student class

---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAPI_V1.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+        public string Email { get; set; }
+    }
+}
