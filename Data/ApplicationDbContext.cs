137e52821008795ac913799e82b4fa74e0a140ad
Motjiang <motjiangbrilliant@gmail.com>
2024-07-26 14:04:23 +0200

    connection string

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAPI_V1.Models;
+
+namespace StudentAPI_V1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
